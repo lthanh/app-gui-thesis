d50162aefa04c7a39b6eadeafbb178c4fabfdcb6
lequocthanh310@gmail.com <lequocthanh310@gmail.com@68437364-25d3-ab4c-04bb-e87548b19c5c>
2014-01-16 16:00:47 +0000

    - hoan thanh V6 va chia package

---
+++ trunk/architecture/Friends.java
@@ -0,0 +1,34 @@
+package architecture;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author admin
+ */
+public class Friends extends UserLoginObject {
+
+    public Friends() {
+    }
+    IPAddress ip;
+    public int countOffline;
+
+    public IPAddress getIp() {
+        return ip;
+    }
+
+    public void setIp(IPAddress ip) {
+        this.ip = ip;
+    }
+
+    public int getCountOffline() {
+        return countOffline;
+    }
+
+    public void setCountOffline(int countOffline) {
+        this.countOffline = countOffline;
+    }
+}
---
+++ trunk/architecture/NetworkManager.java
@@ -0,0 +1,89 @@
+package architecture;
+
+/*
+ Network Manager - started by main
+ */
+
+import java.net.*;
+import java.io.*;
+import java.util.Arrays;
+
+public class NetworkManager {
+
+    public static void writeToOne(IPAddress ip, Packet packet) {
+        if (HostArray.isLive(ip)) {
+            Connection connection = HostArray.getConnection(ip);
+            try {
+                connection.getByteWriter().write(packet.contents(), 0, packet.totalLength());
+                connection.getByteWriter().flush();
+            } catch (IOException e) {
+                try {
+                    connection.getSocket().close();
+                    HostArray.removeConnection(ip);
+                } catch (IOException exception) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void writeToAll(Packet packet) {
+        for (int i = 0; i < HostArray.getCount(); i++) {
+            Connection c = HostArray.getConnection(i);
+            try {
+                if (packet.getPayload() == 2) {
+                    System.out.println("PACKET CONTENT: Payload - " + packet.getPayload() + " Content: " + packet.contents().toString());
+                    System.out.println("PACKET TOTAL LENGTH: " + packet.totalLength());
+                    System.out.println("PACKET Content LENGTH: " + packet.contents.length);
+//                    for (int j = 0; j < packet.contents.length; j++) {
+//                        System.out.println("CONTEN PACKET NETWORK MANAGER [" + j + "]" + packet.contents[j]);
+//                    }
+                }
+
+                c.getByteWriter().write(packet.contents(), 0, packet.totalLength());
+                c.getByteWriter().flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+                try {
+                    c.getSocket().close();
+                    HostArray.removeConnection(c);
+                } catch (IOException exception) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void writeButOne(IPAddress ip, Packet packet) {
+        for (int i = 0; i < HostArray.getCount(); i++) {
+            Connection c = HostArray.getConnection(i);
+            if (!(c.compareConnections(ip))) {
+                try {
+                    c.getByteWriter().write(packet.contents(), 0, packet.totalLength());
+                    c.getByteWriter().flush();
+                } catch (IOException e) {
+                    try {
+                        c.getSocket().close();
+                        HostArray.removeConnection(c);
+                    } catch (IOException exception) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public static void notify(IPAddress ip) // Remove socket from open connection list, based on its IP.
+    {
+        if (HostArray.isLive(ip)) {
+            System.out.println("Killing " + ip);
+            Connection c = HostArray.getConnection(ip);
+            try {
+                c.getSocket().close();
+                HostArray.removeConnection(c);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
